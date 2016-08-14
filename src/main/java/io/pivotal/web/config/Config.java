173371f2e9984feb10b60e8c110794471f6fb241
Carlos Queiroz <caxqueiroz@gmail.com>
2016-08-15 06:52:07 +0800

    First commit.

---
+++ src/main/java/io/pivotal/web/config/Config.java
@@ -0,0 +1,18 @@
+package io.pivotal.web.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by cq on 15/8/16.
+ */
+@Configuration
+public class Config {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
